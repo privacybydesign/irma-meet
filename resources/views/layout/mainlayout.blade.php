a71a23e1b1d034ada64e396e3b6b73c62f97ca83
Hanna <git@creativecode.org>
2020-05-17 11:54:44 +0200

    constraining width of the content for wide screens
    CSS improvements and visual design improvements. Mainly constraining the content in the info-rows so it cannot be wider than 1200px. Replaced image.

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -42,7 +42,7 @@
         </main>
         <main class="col-md-4 bg-warmwhite">
         	<div class="content">
-        		<p class="cl-grey">AUTHENTICATED VIDEO CALLS</p>
+        		<p class="cl-grey slogan">AUTHENTICATED VIDEO CALLS</p>
         		<h1>Meet online, without surprises!</h1>
         		<p>Only people who prove who they are can participate in IRMA-meet video. Chat with certainty, without being fooled, in your business meeting, (medical) consult, or online oral exam.</p>
                 <div class="">
@@ -56,51 +56,65 @@
         </main>
     </div>
 
-    <div class="row img-warp">
-        <main class="col-md-7 bg-grey" >
-            <div class="info">
-                <h2>Know who you are talking to</h2>
-                <p>Participants in IRMA-meet video calls need to prove who they are by disclosing relevant information (like their name and e-mail address) about themselves before they can enter a video chat.</p>
-            </div>
-        </main>
-        <main class="col-md-5 bg-grey" >
-            <div class="img-warp">
-                <img class="img-fluid know" src="{{ url('/') }}/img/know.svg" />
-            </div>
-        </main>
-    </div>
-    <div class="row img-warp">
-        <div class="row img-warp">
-            <main class="col-md-5 bg-light-grey" >
-                <div class="col-sm-5"><img class="img-fluid prove" src="{{ url('/') }}/img/prove.svg" /></div>
-                <div class="col-sm-7"><img class="img-fluid irma_logo_info" src="{{ url('/') }}/img/irma_logo.svg" /></div>
+    <div class="row-full bg-grey">
+        <div class="row img-warp tight">
+            <main class="col-md-7 bg-grey" >
+                <div class="info">
+                    <h2>Know who you are talking to</h2>
+                    <p>Participants in IRMA-meet video calls need to prove who they are by disclosing relevant information (like their name and e-mail address) about themselves before they can enter a video chat.</p>
+                </div>
+            </main>
+            <main class="col-md-5 bg-grey" >
+                <div class="img-warp">
+                    <img class="img-fluid know" src="{{ url('/') }}/img/know.svg" />
+                </div>
             </main>
         </div>
-        <main class="col-md-7 bg-light-grey" >
-            <div class="info">
-                <h2>Prove who you are with IRMA</h2>
-                <p>You can prove who you are via <a href="https://privacybydesign.foundation">IRMA</a>. It is a freely available open source identity app; it is a wallet on your phone for collecting personal data. This data is stored only on your phone, and nowhere else. You control where you disclose it.</p>
-            </div>
-        </main>
-    </div>
-    <div class="row">
-        <main class="col-md-7" >
-            <div class="info">
-                <div>
-                    <h2>Get ready</h2>
-                </div>
-                <p>All IRMA-meet users need to have the IRMA app on their phone, filled with the relevant personal data. Which data is required may depend on the video call.</p>
-                <p>If you don't have IRMA yet, install it from the IOS appstore or Google Play store and load the required personal data, like your name or e-mail address.</p>
-                <p>At this early stage, there is only one type of free meeting available, which requires your e-mail address and your name in IRMA. Your name can be obtained either from your LinkedIn profile or for users in the Netherlands from the Dutch citizen registry.</p>
-            </div>
-        </main>
-        <main class="col-md-5" >
-            <div class="">
-                <img class="img-fluid " src="{{ url('/') }}/img/getready.svg" />
-            </div>
-        </main>
     </div>
     
+    <div class="row-full bg-grey bg-light-grey">
+        <div class="row img-warp tight">
+            <main class="col-md-5">
+                <div class="">
+                    <img class="img-fluid irma_logo_info" src="{{ url('/') }}/img/irma_logo.svg" />
+                </div>
+            </main>
+            <main class="col-md-7">
+                <div class="info">
+                    <h2>Prove who you are with IRMA</h2>
+                    <p>You can prove who you are via <a href="https://privacybydesign.foundation">IRMA</a>. It is a
+                        freely available open source identity app; it is a wallet on your phone for collecting personal
+                        data. This data is stored only on your phone, and nowhere else. You control where you disclose
+                        it.</p>
+                </div>
+                <div>
+                    <img class="img-fluid irma_logo_small" src="{{ url('/') }}/img/irma_logo.svg" />
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <div class="row img-warp tight">
+            <main class="col-md-7">
+                <div class="info">
+                    <div>
+                        <h2>Get ready</h2>
+                    </div>
+                    <p>All IRMA-meet users need to have the IRMA app on their phone, filled with the relevant personal
+                        data. Which data is required may depend on the video call.</p>
+                    <p>If you don't have IRMA yet, install it from the IOS appstore or Google Play store and load the
+                        required personal data, like your name or e-mail address.</p>
+                    <p>At this early stage, there is only one type of free meeting available, which requires your e-mail
+                        address and your name in IRMA. Your name can be obtained either from your LinkedIn profile or
+                        for users in the Netherlands from the Dutch citizen registry.</p>
+                </div>
+            </main>
+            <main class="col-md-5">
+                <div class="">
+                <img class="img-fluid getready" src="{{ url('/') }}/img/getready.svg" />
+                </div>
+            </main>
+        </div>    
 
 </div>
 
