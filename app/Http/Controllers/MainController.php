995180b153d2ff538a2d9b4c4e73d45e32db3151
Joeri de Bruin <joeri@oqapi.nl>
2020-06-26 18:41:39 +0200

    start sending e-mails to participants

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,7 +25,7 @@ class MainController extends Controller
     {
         $version = \Tremby\LaravelGitVersion\GitVersionHelper::getVersion();
         return view('layout/mainlayout')->with([
-            'message' => '<img class="img-fluid" src="'.url('/') .'/img/IRMA-meet.png" alt="IRMA-meet logo v' . $version . '">',
+            'message' => '<img class="img-fluid" src="'.url('/') .'/img/IRMA-meet.png" alt="IRMA-meet logo v.' . $version . '">',
             'title' => __('welcome'),
             'buttons' => '<button type="button" class="btn btn-primary btn-lg btn-blue" onclick="startInvitation(\'./irma_auth/start/default\', \'./irma_session/create/free\');">' . __('Start a free meeting now') . '</button> <button type="button" class="btn btn-primary btn-lg btn-blue" onclick="startInvitation(\'./irma_auth/start/medical\', \'./irma_session/create/medical_consult\');">' . __('Start a medical consult now') . '</button><button type="button" class="btn btn-primary btn-lg btn-blue" onclick="startInvitation(\'./irma_auth/start/teacher\', \'./irma_session/create/exam\');">' . __('Start an exam now') . '</button>'
         ]);
