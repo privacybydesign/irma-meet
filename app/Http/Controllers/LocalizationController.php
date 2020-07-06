e19beb4dad014e83c3a1e30656aa453c18f406f5
Joeri de Bruin <joeri@oqapi.nl>
2020-07-06 08:24:53 +0200

    fixed language switcher

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -14,6 +14,6 @@ class LocalizationController extends Controller
         \App::setLocale($locale);
         //storing the locale in session to get it back in the middleware
         session()->put('locale', $locale);
-        return redirect()->back();
+        return redirect()->home();
     }
 }
