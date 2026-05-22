6edbc9746275fc7818beddb524bdc3f3e1f0d75f
Ruben Hensen <ruben.hensen@protonmail.com>
2026-05-22 14:33:56 +0200

    fix: hide medical consult button (requires deprecated Yivi AGB/BSN credentials)

--- resources/views/layout/partials/buttons.blade.php
+++ resources/views/layout/partials/buttons.blade.php
@@ -6,7 +6,4 @@
 <button type="button" class="btn btn-secondary btn-lg btn-blue" onclick="startInvitation('{{ url('/') }}/irma_auth/start/teacher', '{{ url('/') }}/irma_session/create/exam');">
 	<img class="img-fluid" src="{{ url('/') }}/img/exam-icon.svg">{{ __('Start oral video exam') }}
 </button>
-<br><br>
-<button type="button" class="btn btn-secondary btn-lg btn-blue" onclick="startInvitation('{{ url('/') }}/irma_auth/start/medical', '{{ url('/') }}/irma_session/create/medical_consult');">
-	<img class="img-fluid" src="{{ url('/') }}/img/medical_icon.svg">{{ __('Start medical video consult') }}
-</button>
+{{-- Medical consult button hidden: requires deprecated Yivi credentials (AGB-code/BSN) --}}
